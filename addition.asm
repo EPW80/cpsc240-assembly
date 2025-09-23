d7679a859ed1dfb1a124152c097ea66cf4ae4f76
Erik Williams <epwilliams@csu.fullerton.edu>
2025-09-22 21:52:02 -0700

    convert addition back to 16-bit

--- addition.asm
+++ addition.asm
@@ -1,20 +1,19 @@
 ; addition.asm
 ; unsigned short num1 = 65244, num2 = 4660;
-; unsigned int sum = 0;
-; sum = int(num1 + num2);
+; unsigned short sum = 0;
+; sum = num1 + num2;
 section .data
 SYS_exit equ 60
 EXIT_SUCCESS equ 0
 num1 dw 65244 ;num1 = FEDCh
 num2 dw 4660 ;num2 = 1234h
-sum dd 0 ;sum = 00000000h
+sum dw 0 ;sum = 0000h
 section .text
 global _start
 _start:
-movzx eax, word[num1] ;eax = 0000FEDCh
-movzx ebx, word[num2] ;ebx = 00001234h
-add eax, ebx ;eax = eax + ebx = 00011110h
-mov dword[sum], eax ;[sum] = eax = 00011110h
+mov ax, word[num1] ;ax = num1 = FEDCh
+add ax, word[num2] ;ax = ax + num2 = 1110h (with overflow)
+mov word[sum], ax ;[sum] = ax = 1110h
 mov rax, SYS_exit ;terminate excuting process
 mov rdi, EXIT_SUCCESS ;exit status
 syscall ;calling system services
